c3bb26ee4f1cd71a10ca58cfb5e18d911f4ea00f
LeeSeungHun <dos07008@naver.com>
2023-12-27 02:30:31 +0900

    feat: yarn berry 세팅, eslint, prettier 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+};
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+
+const test = () => {
+  console.log('test');
+};
+
+const b = 'a'
+const a = 'a';
